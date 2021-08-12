4c7d4627bc55fb8f3f44e0f32cba24db5ae9d057
Blaine Booher <blaine@emergingprairie.com>
2021-08-12 11:14:03 -0500

    updated -- in the future, just run update.sh and manually clear newlines from a dozen or so entries in data/gardens.csv

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "Downloading csv data..."
-curl https://docs.google.com/spreadsheets/d/13wjQS8ZfO7rA30p16btGJEaGitxJBjJMCLMYirhH8RA/pub?output=csv > data/gardens.csv
+#curl https://docs.google.com/spreadsheets/d/13wjQS8ZfO7rA30p16btGJEaGitxJBjJMCLMYirhH8RA/pub?output=csv > data/gardens.csv
+#curl https://doc-0c-9c-sheets.googleusercontent.com/export/l5l039s6ni5uumqbsj9o11lmdc/uvuqmb1s5jb9m5nvh3n5ntq29k/1617920450000/104151212979487901196/*/19DtK8BtamgFz7QtDUB4FvcOMgMLr7jd3vZWKgBhdcPg?format=csv > data/gardens.csv
+curl -L https://docs.google.com/spreadsheets/d/19DtK8BtamgFz7QtDUB4FvcOMgMLr7jd3vZWKgBhdcPg/export?format=csv > data/gardens.csv
 echo "Syncing with github..."
 echo "Done! Run the following to push to the web:"
 echo "git add data/gardens.csv"
